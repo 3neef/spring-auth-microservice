86c508828d7f9b00a18d94459bcf6bef5d21990d
Mazin Hafiz <hajiki.326@hotmail.com>
2023-03-11 21:38:54 +0200

    :white_check_mark: add the security config

---
+++ src/main/java/com/microservices/micro/security/WebSecurityConfig.java
@@ -0,0 +1,7 @@
+package com.microservices.micro.security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+public class WebSecurityConfig {
+    
+}
